1b3fbede5149f350cec226c48f1c599a29172569
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-05-18 22:27:08 -0400

    updated the user api-ep

--- src/main/java/com/example/api/HelloController.java
+++ src/main/java/com/example/api/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,11 @@ public class HelloController {
 	public String Greet() {
 		return "Welcome to OAuth2";
 	}
+	
+	@GetMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+	    return principal.getAttributes().toString();
+	}
+
 	
 }
